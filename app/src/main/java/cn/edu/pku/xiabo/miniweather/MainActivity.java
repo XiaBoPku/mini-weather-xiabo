9a56e8f785968b93f4da0960152a494b9f5e915c
xiabo <504761767@qq.com>
2017-09-21 10:14:29 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/xiabo/miniweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.xiabo.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
