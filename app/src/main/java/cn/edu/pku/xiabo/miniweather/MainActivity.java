843f42d576e16e76b460940f54aca464ad6c1019
xiabo <504761767@qq.com>
2017-10-11 07:36:53 +0800

    add judge network state

--- app/src/main/java/cn/edu/pku/xiabo/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/xiabo/miniweather/MainActivity.java
@@ -1,14 +1,91 @@
 package cn.edu.pku.xiabo.miniweather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
+    ImageView mUpdateBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+        mUpdateBtn = (ImageView)findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("config",MODE_PRIVATE);
+                String cityCode = sharedPreferences.getString("main_city_code","101010100");
+
+                if (NetUtil.getNetworkState(MainActivity.this) != NetUtil.NETWORN_NONE) {
+                    Log.d("myWeather", "网络OK");
+                    Toast.makeText(MainActivity.this,"heihei",Toast.LENGTH_SHORT).show();
+                    queryWeatherCode(cityCode);
+                }else
+                {
+                    Log.d("myWeather", "网络挂了");
+
+                }
+            }
+        });
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this,"网络OK！", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了！", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+            * *
+    @param cityCode
+*/
+    private void queryWeatherCode(String cityCode) {
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d("myWeather", address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con=null;
+                try{
+                    URL url = new URL(address);
+                    con = (HttpURLConnection)url.openConnection(
+                    );
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while((str=reader.readLine()) != null){
+                        response.append(str);
+                        Log.d("myWeather", str);
+                    }
+                    String responseStr=response.toString();
+                    Log.d("myWeather", responseStr);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
     }
 }
